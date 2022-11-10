887b339a1aaab0fe3c59e0acb25bb75274c0e9c3
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2022-11-10 22:43:23 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let askCity = prompt('Enter a city?');
 
 let paris =  {
         name: 'Paris',
-        temp: Math.round(29.5),
+        temp: Math.round(29.5),        
         humidity: '60%'
     } ;
 let tokyo = {
@@ -16,14 +16,22 @@ let kiev = {
         temp: Math.round(30.7),
         humidity: '70%' 
     };
-  
+
+let temperatureParis = paris.temp;
+let fahreinheitTempParis =  ( temperatureParis * 9 / 5 ) + 32;
+
+let temperatureTokyo = tokyo.temp;
+let fahreinheitTempTokyo =  ( temperatureTokyo * 9 / 5 ) + 32;
+
+let temperatureKiyv = kiev.temp;
+let fahreinheitTempKiyv =  ( temperatureKiyv * 9 / 5 ) + 32;
 
 if(askCity == 'paris') {
-    alert(`It is currently ${paris.temp}°C in ${paris.name} with a humidity of ${paris.humidity}`);    
+    alert(`It is currently ${paris.temp}°C (${fahreinheitTempParis}°F) in ${paris.name} with a humidity of ${paris.humidity}`);    
 }else if(askCity == 'tokyo') {
-    alert(`It is currently ${tokyo.temp}°C in ${tokyo.name} with a humidity of ${tokyo.humidity}`);
-}else if( askCity == 'Kiyv') {
-    alert(`It is currently ${kiev.temp}°C in ${kiev.name} with a humidity of ${kiev.humidity}`);
+    alert(`It is currently ${tokyo.temp}°C (${fahreinheitTempTokyo}°F) in ${tokyo.name} with a humidity of ${tokyo.humidity}`);
+}else if( askCity == 'kiyv') {
+    alert(`It is currently ${kiev.temp}°C (${fahreinheitTempKiyv}°F) in ${kiev.name} with a humidity of ${kiev.humidity}`);
 }else {
     alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${askCity}`);
 };
