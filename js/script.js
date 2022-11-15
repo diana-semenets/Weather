5f795a6f7826629fdea926800c9dbee4578b3b00
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2022-11-15 23:47:48 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-let askCity = prompt('Enter a city?');
+/*let askCity = prompt('Enter a city?');
 
 let paris =  {
         name: 'Paris',
@@ -34,4 +34,66 @@ if(askCity == 'paris'|| askCity == 'Paris' || askCity == 'PARIS') {
     alert(`It is currently ${kiev.temp}°C (${fahreinheitTempKiyv}°F) in ${kiev.name} with a humidity of ${kiev.humidity}`);
 }else {
     alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${askCity}`);
-};
+};   */
+
+//----------------------//
+
+let now = new Date();
+
+      let span = document.querySelector(".place-span");
+
+      let date = now.getDate();
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      let year = now.getFullYear();
+
+      let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      let day = days[now.getDay()];
+
+      let months = [
+        "Jan",
+        "Feb",
+        "March",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+      let month = months[now.getMonth()];
+
+      span.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}, ${year}`;
+
+//----------------------//
+
+
+function getPlace (event) {
+    event.preventDefault(); 
+    let place = document.querySelector('#main__input'); 
+    let mainPlace = document.querySelector('.main__place-place');
+    //console.log('ok');
+    mainPlace.innerHTML = place.value;
+    
+}
+let form = document.querySelector("#form");
+
+form.addEventListener("submit", getPlace);
+
+//----------------------//
+
+let temperature = 17;
+
+function showTempFarent (event) {
+    event.preventDefault();
+    let temp = document.querySelector('.deg-number');
+    let tempFaren = Math.round( temperature * 9 / 5 ) + 32;
+    temp.innerHTML = tempFaren;
+    console.log('ok')
+}
+showTempFarent();
+// let fahrenheit = document.querySelector('#fahrenheit');
+// fahrenheit.addEventListener('click', showTempFarent);
